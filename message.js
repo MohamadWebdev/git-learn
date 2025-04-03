36e06b0cd192104bb481557935fd84ee9b6507d9
MohamadWebdev <mohamad.webdev99@gmail.com>
2025-04-03 13:02:01 -0700

    new value branch 2 of branch 2

--- message.js
+++ message.js
@@ -2,4 +2,4 @@ let user = "John doe"
 let message = "Hello, my name is " + user + ". Nice to meet you!"
 console.log(message)
 
-// let user = "new value branch 2"
+// let user = "new value branch 2 of branch 2"
